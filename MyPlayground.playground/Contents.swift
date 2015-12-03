a4fb6c5c26aff466ba790952253f1876f0fc14e6
caorpr <caorpr@gmail.com>
2015-12-03 13:57:30 -0700

    fdasfdasfdsa

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let PLAYGROUND : String = "learning swift"
+var myWords = PLAYGROUND
+myWords = "new stuff"
+print(myWords)
+print(PLAYGROUND)
